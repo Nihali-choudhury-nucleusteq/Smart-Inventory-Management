1637d205d16e7d724e98c14cc69c678b4088d63b
Nihali96 <nihali.choudhury@nucleusteq.com>
2025-04-09 14:13:58 +0530

    controller implementation

--- src/main/java/com/Inventory/order/controller/OrderController.java
+++ src/main/java/com/Inventory/order/controller/OrderController.java
@@ -1,8 +1,24 @@
 package com.Inventory.order.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Inventory.order.entity.Orders;
+import com.Inventory.order.service.OrdersSrevice;
 
 @RestController
 public class OrderController {
+	
+	@Autowired
+	OrdersSrevice ordersSrevice;
+	
+	@PostMapping("/addorder")
+    public Orders createOrders(@RequestBody Orders order) {
+		Orders orders = ordersSrevice.createOrders(order);
+		return orders;
+	}
+	
 
 }
--- src/main/java/com/Inventory/order/controller/OrderItemsController.java
+++ src/main/java/com/Inventory/order/controller/OrderItemsController.java
@@ -1,8 +1,18 @@
 package com.Inventory.order.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Inventory.order.service.OrderItemsService;
 
 @RestController
 public class OrderItemsController {
+	
+	
+	@Autowired
+	OrderItemsService orderItemsService;
+	
+	
+  
 
 }
