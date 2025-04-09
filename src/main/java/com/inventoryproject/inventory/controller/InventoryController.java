aee55d2fdc71eef61f0d41571069b72664b6e2e6
Nihali96 <nihali.choudhury@nucleusteq.com>
2025-04-09 14:15:29 +0530

    controller implemention

--- src/main/java/com/inventoryproject/inventory/controller/InventoryController.java
+++ src/main/java/com/inventoryproject/inventory/controller/InventoryController.java
@@ -1,23 +1,29 @@
 package com.inventoryproject.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inventoryproject.inventory.entity.Inventory;
+import com.inventoryproject.inventory.service.InventoryService;
 
 @RestController
 public class InventoryController {
-	@GetMapping("/stocksavail")
+	@Autowired
+	InventoryService inventoryService;
+	@GetMapping("/stocksavail/{productId}")	
     public int getStockAvailability(@PathVariable String productId) {
-		
-		return 0;
+		int result = inventoryService.getStockAvailability(productId);
+		return result;
 	}
 
-	
-	public Inventory updateStocks(String productId, int stock) {
-		
-		return null;
+	@PutMapping("/updatestock/{productId}/{quantity}")
+	public Inventory updateStocks(@RequestBody Inventory inv, @PathVariable String productId,@PathVariable int quantity) {
+		Inventory  inventory = inventoryService.updateStocks(productId, quantity);
+		return inventory;
 	}
 
 }
